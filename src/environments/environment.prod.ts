4c77e6cc7cdd83b1dda47321302493173d7665a2
Matheus <matheus_mgp@hotmail.com>
2025-06-23 13:42:38 -0300

    https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://sua-api-producao.com/api',
   sgiURL: 'http://sv1.flexgrupo.com:9001/',
-  positionsURL: 'http://sv1.flexgrupo.com:9010/api/v1/',
+  positionsURL: 'https://sv1.flexgrupo.com:9010/api/v1/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   sgiURL: 'http://sv1.flexgrupo.com:9001/',
-  positionsURL: 'http://sv1.flexgrupo.com:9010/api/v1/',
+  positionsURL: 'https://sv1.flexgrupo.com:9010/api/v1/',
   suntechWsUrl: 'ws://localhost:3001',
   obdWsUrl: 'ws://localhost:3003',
 };
